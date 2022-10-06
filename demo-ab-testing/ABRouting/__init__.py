e618001be4bec6ef841a5ed094cf36f3213702d2
Arne Mueller <mueller_arne@gmx.net>
2022-10-06 13:51:09 +0000

    add simple routing

--- demo-ab-testing/ABRouting/__init__.py
+++ demo-ab-testing/ABRouting/__init__.py
@@ -1,6 +1,12 @@
 import logging
-
+import requests
 import azure.functions as func
+
+NAME_DICT = {
+    "arne": "model1",
+    "basak": "model2"
+}
+
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
@@ -16,7 +22,17 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             name = req_body.get('name')
 
     if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+        model_version = NAME_DICT.get(name, "model1")
+        # api-endpoint
+        URL = f"https://{model_version}.k8s.arnemue.de"
+        # sending get request and saving the response as response object
+        r = requests.get(url = URL)
+
+        data = r.json()
+
+        group = data["Group"]
+
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully and the group is {group}.")
     else:
         return func.HttpResponse(
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
